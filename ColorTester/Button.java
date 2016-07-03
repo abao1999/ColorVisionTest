0a1d3c591f480cbbb6d6fa59fede6a575af61111
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 19:45:09 -0700

    Update Button.java

--- ColorTester/Button.java
+++ ColorTester/Button.java
@@ -1,25 +1,18 @@
-/*    */ package ColorTester;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Font;
-/*    */ import javax.swing.JButton;
-/*    */ 
-/*    */ public class Button extends JButton
-/*    */ {
-/*  9 */   Font font = new Font("Verdana", 1, 15);
-/*    */ 
-/*    */   public Button(Color color)
-/*    */   {
-/* 13 */     setVerticalTextPosition(0);
-/* 14 */     setHorizontalTextPosition(0);
-/* 15 */     setFont(this.font);
-/* 16 */     setBackground(color);
-/* 17 */     setOpaque(true);
-/* 18 */     setBorderPainted(false);
-/*    */   }
-/*    */ }
+package ColorTester;
 
-/* Location:           /Users/timcoker/Desktop/ColorVisionTest.jar
- * Qualified Name:     ColorTester.Button
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Button extends JButton {
+  Font font = new Font("Verdana", 1, 15);
+
+  public Button(Color color)  {
+    setVerticalTextPosition(0);
+    setHorizontalTextPosition(0);
+    setFont(this.font);
+    setBackground(color);
+    setOpaque(true);
+    setBorderPainted(false);
+  }
+}
