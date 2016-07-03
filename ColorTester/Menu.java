f1985359a681543021fb6eaafdaa8bdc782f2959
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 19:48:20 -0700

    Update Menu.java

--- ColorTester/Menu.java
+++ ColorTester/Menu.java
@@ -1,17 +1,10 @@
-/*   */ package ColorTester;
-/*   */ 
-/*   */ import java.awt.Color;
-/*   */ import javax.swing.JPanel;
-/*   */ 
-/*   */ public class Menu extends JPanel
-/*   */ {
-/*   */   public Menu()
-/*   */   {
-/* 9 */     setBackground(Color.YELLOW);
-/*   */   }
-/*   */ }
+package ColorTester;
 
-/* Location:           /Users/timcoker/Desktop/ColorVisionTest.jar
- * Qualified Name:     ColorTester.Menu
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel  {
+  public Menu() {
+    setBackground(Color.YELLOW);
+  }
+}
