75166ffc78d188fa35e64c235755530fd4ca2730
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 19:45:39 -0700

    Update Header.java

--- ColorTester/Header.java
+++ ColorTester/Header.java
@@ -1,24 +1,18 @@
-/*    */ package ColorTester;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Dimension;
-/*    */ import javax.swing.JPanel;
-/*    */ import javax.swing.JTextArea;
-/*    */ 
-/*    */ public class Header extends JPanel
-/*    */ {
-/* 11 */   static JTextArea texta = new JTextArea("Ready Player One");
-/*    */ 
-/*    */   public Header() {
-/* 14 */     texta.setBackground(Color.YELLOW);
-/* 15 */     texta.setEditable(false);
-/* 16 */     setPreferredSize(new Dimension(Page.size.width, 30));
-/* 17 */     setBackground(Color.YELLOW);
-/* 18 */     add(texta);
-/*    */   }
-/*    */ }
+package ColorTester;
 
-/* Location:           /Users/timcoker/Desktop/ColorVisionTest.jar
- * Qualified Name:     ColorTester.Header
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class Header extends JPanel  {
+  static JTextArea texta = new JTextArea("Ready Player One");
+
+  public Header() {
+    texta.setBackground(Color.YELLOW);
+    texta.setEditable(false);
+    setPreferredSize(new Dimension(Page.size.width, 30));
+    setBackground(Color.YELLOW);
+    add(texta);
+  }
+}
